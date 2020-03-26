3af6ba4fd6f0365d2b4f6aac9f723cc4a9b52f64
qy9708 <tan2tan1997@gmail.com>
2020-03-26 17:51:18 +0800

    y

--- database/migrations/2020_03_26_081030_create_publishers_table.php
+++ database/migrations/2020_03_26_081030_create_publishers_table.php
@@ -21,8 +21,8 @@ class CreatePublishersTable extends Migration
             $table->increments('id');
             $table->string('name', 150);
             $table->string('address', 150);
-            $table->integer('year', 4);
-            $table->integer('edition', 2);
+            $table->integer('year')->length(4);
+            $table->integer('edition')->length(2);
             $table->string('email', 50);
             $table->timestamps();
         });
