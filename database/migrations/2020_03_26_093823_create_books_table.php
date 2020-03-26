91a7236b0641ed2484916c3f092c4579a5861880
qy9708 <tan2tan1997@gmail.com>
2020-03-26 17:42:10 +0800

    -

--- database/migrations/2020_03_26_093823_create_books_table.php
+++ database/migrations/2020_03_26_093823_create_books_table.php
@@ -33,11 +33,6 @@ class CreateBooksTable extends Migration
          });
 
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('books');
