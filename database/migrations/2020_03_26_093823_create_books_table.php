6a07df6b5ffd1fecbc776f915e63e85c8f479045
qy9708 <tan2tan1997@gmail.com>
2020-03-26 17:40:10 +0800

    -

--- database/migrations/2020_03_26_093823_create_books_table.php
+++ database/migrations/2020_03_26_093823_create_books_table.php
@@ -11,13 +11,28 @@ class CreateBooksTable extends Migration
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('books', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
-    }
+     public function up()
+     {
+         Schema::enableForeignKeyConstraints();
+         Schema::dropIfExists('books');
+
+         Schema::create('books', function (Blueprint $table) {
+             $table->engine='InnoDB';
+             $table->increments('id');
+             $table->binary('image')->nullable();
+             $table->string('title', 150);
+             $table->string('category', 100);
+             $table->string('status', 20);
+             $table->unsignedInteger('author_id')->nullable();
+             $table->unsignedInteger('publisher_id')->nullable();
+
+             $table->foreign('author_id')
+                   ->references('id')->on('authors');
+             $table->foreign('publisher_id')
+                   ->references('id')->on('publishers');
+             $table->timestamps();
+         });
+
 
     /**
      * Reverse the migrations.
