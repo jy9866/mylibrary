218aef0c352d44298466e597495e7387d6e6d026
qy9708 <tan2tan1997@gmail.com>
2020-03-30 16:44:06 +0800

    -

--- database/migrations/2020_03_26_093823_create_books_table.php
+++ database/migrations/2020_03_26_093823_create_books_table.php
@@ -22,14 +22,15 @@ class CreateBooksTable extends Migration
              $table->string('title', 150);
              $table->string('category', 100);
              $table->string('status', 20);
-             $table->unsignedInteger('author_id')->nullable();
-             //$table->unsignedInteger('publisher_id')->nullable();
+             $table->unsignedInteger('author_id')->nullable($value = false);
+             $table->unsignedInteger('publisher_id')->nullable($value = false);
+
              $table->timestamps();
 
              $table->foreign('author_id')
                    ->references('id')->on('authors');
-             /*$table->foreign('publisher_id')
-                   ->references('id')->on('publishers');*/
+             $table->foreign('publisher_id')
+                   ->references('id')->on('publishers');
 
          });
       }
